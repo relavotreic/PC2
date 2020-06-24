3d200190b9f8f34cb2c1a1644abb0613919afea7
relavotreic <66016125+relavotreic@users.noreply.github.com>
2020-06-24 08:02:40 -0500

    pc2 ejercicio 2

---
+++ ejer2.py
@@ -0,0 +1,21 @@
+# Crear una función para rellenar un arreglo tipo lista 2-D en el que se solicite el número de filas y
+# columnas, se rellena el arreglo con número aleatorios.
+# NOTA: No utilizar Numpy para esta pregunta
+#  Nombre de función: crea_arreglo
+#  Entradas: 2
+# o filas
+# o columnas
+#  Salida:
+# o Arreglo con números aleatorios
+from random import randint as ri
+def crea_arreglo(f,c):
+    lista=[]
+    # no se especifica si desea que ingrese  algun dato en el ejercicio
+    f=int(input("f="))
+    c=int(input("c="))
+    for i in range (f):
+        a=[0]*c
+        lista.append(a)
+        for j in range(c):
+            lista[i][j]=ri(100,900)
+    
